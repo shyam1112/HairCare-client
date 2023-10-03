ed86c53443afbe6b294e4a24251ed38ccc95aca6
shyam sardhara <80083773+shyam1112@users.noreply.github.com>
2023-10-03 23:16:42 +0530

    update

--- src/Component/customerlogin/Cushome/Cushome.js
+++ src/Component/customerlogin/Cushome/Cushome.js
@@ -2,12 +2,16 @@ import React, { useEffect, useState } from 'react';
 import Cusnavbar from '../Navbar/Cusnavbar';
 import './cushome.css';
 import Accordion from 'react-bootstrap/Accordion';
+import { ToastContainer, toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
+
 
 function Cushome() {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
-
+  const [requestSent, setRequestSent] = useState(false);
+  const [rsp,setRsp]=useState('Best of the luck');
   const [time, setTime] = useState({
     hour: 10,
     minute: 0,
@@ -65,9 +69,67 @@ function Cushome() {
     }
   };
 
-  const sendreq=()=>{
-    const timee =time.hour+':'+time.minute+':'+time.ampm
-    console.log(timee);
+const navigate=useNavigate();
+
+  const sendreq=async(userId)=>{
+    // console.log(userId);
+    let idd='';
+    const timee =time.hour+':'+time.minute+':'+time.ampm;
+    const reqee=false;
+    try {
+      const response = await fetch('http://localhost:4000/sendreq', {
+        method: 'POST',
+        body: JSON.stringify({userId,timee,reqee}),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        idd=data.id;
+        // console.log(data.id)
+          toast.success("Request send Successfully...done 😃!", {
+              position: "top-center"
+          });
+          // setTimeout(()=>{
+          //   navigate('/cushome');
+          // },'2000');
+      //   console.log('Data sent successfully');
+      } else {
+          toast.error("Request failed. Please try again later.", {
+              position: "top-center"
+          });
+      //   console.error('Error sending data');
+      }
+    } catch (error) {
+      toast.error("Error sending data", {
+          position: "top-center"
+      });
+      // console.error('Error sending data', error);
+    }
+    setRequestSent(true);
+
+    setTimeout(async () => {
+      try {
+        let result = await fetch(`http://localhost:4000/accept/${idd}`);
+        if (!result.ok) {
+          throw new Error('Network response was not ok');
+        }
+        result = await result.json();
+        // console.log(result.reqee);
+        if (result.reqee == true) {
+          setRsp("Confirm");
+        } else {
+          setRsp("Select Another time..");
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      } finally {
+        setRequestSent(false);
+      }
+    }, 10000);
+    
   }
 
   return (
@@ -91,7 +153,7 @@ function Cushome() {
                     <Accordion.Header>Shop Name : {item.shopname}</Accordion.Header>
                     <Accordion.Body>
                       <p>Owner : {item.owner}</p>
-                      <p>Id : {item.userId}</p>
+                      <p>Id : {item._id}</p>
                       <p>Mobile Number : {item.mobilenumber}</p>
                       <p>Address : {item.address}</p>
                       <div className='timepicker'>
@@ -123,9 +185,16 @@ function Cushome() {
                           </select>
                         </label>
                       </div>
+                      {requestSent?("Waiting.."):
+                      <>
                       <div className='btnreq'>
-                        <button onClick={sendreq}>Send request</button>{' '}
+                        <button onClick={()=>sendreq(item.userId)}>Send request</button>{' '}
                       </div>
+                       <div className='btnreq'>
+                       <button>{rsp}</button>{' '}
+                     </div>
+                      </>
+                      }
                     </Accordion.Body>
                   </Accordion.Item>
                 </Accordion>
@@ -136,6 +205,7 @@ function Cushome() {
           )}
         </div>
       </div>
+      <ToastContainer/>
     </div>
   );
 }
--- src/Component/shopinfo/Shopdata.js
+++ src/Component/shopinfo/Shopdata.js
@@ -1,15 +1,85 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './shopdata.css'; // Import the CSS file
-
+import { ToastContainer, toast } from 'react-toastify';
 import Nav from '../Nav/Nav';
 
 function ShopForm() {
-  
-    
+
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const auth = localStorage.getItem('userid');
+
+  const getData = async () => {
+    try {
+      let result = await fetch(`http://localhost:4000/getreq/${auth}`);
+      if (!result.ok) {
+        throw new Error('Network response was not ok');
+      }
+      result = await result.json();
+      // console.log(result);
+      setData(result);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      setIsLoading(false);
+      console.error('Error in fetching data:', error);
+    }
+  };
+
+  const acceptreq = async (id) => {
+    let result = await fetch(`http://localhost:4000/update/${id}`, {
+      method: 'put',
+      body: JSON.stringify({ reqee: true }),
+      headers: {
+        'content-type': 'application/json'
+      },
+    })
+    result = await result.json();
+    // console.log(result);
+    toast.success("Request accept..😃!", {
+      position: "top-center"
+  });
+  }
 
   return (
     <div>
-       <Nav/>
+      <Nav />
+      <div className='shopdata-main'>
+
+        {isLoading ? (
+          <div>Loading...</div>
+        ) : error ? (
+          <div>Error: {error.message}</div>
+        ) : data && data.length > 0 ? (
+          data.map((item, index) => (
+            <div className='alertt'>
+              <div className='faltu'>
+                <ul key={item._id}>
+                  <p>{item._id}</p>
+                  &nbsp;  &nbsp;  &nbsp;  &nbsp;
+                  <p>{item.timee}</p>
+                  <div className='accept-req-btn'>
+                    <button onClick={()=> acceptreq(item._id)}>Accept Request</button>
+                  </div>
+                </ul>
+
+              </div>
+            </div>
+          ))
+        ) : (
+          <div>No data available</div>
+        )}
+
+
+<ToastContainer/>
+
+      </div>
 
     </div>
   );
